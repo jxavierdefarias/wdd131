37bdddc75b9cebfb608eb5e6d48cc1f089bffe5a
jxavierdefarias <jxavierdefarias@byupathway.edu>
2026-09-07 14:33:19 -0300

    alt4

--- scripts/obterdatas.js
+++ scripts/obterdatas.js
@@ -1,14 +1,4 @@
-// ano atual
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// ultima modificação em português
-const dataMod = new Date(document.lastModified);
-const formatada = dataMod.toLocaleString("pt-BR", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit"
-});
-document.getElementById("lastModified").textContent = `Última modificação: ${formatada}`;
+const mod = new Date(document.lastModified);
+document.getElementById("lastModified").textContent = `Última modificação: ${mod.toLocaleString("pt-BR")}`;
