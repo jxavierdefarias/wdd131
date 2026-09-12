e83d33601d809bed6b412ec8e9a4c5e687b1a591
jxavierdefarias <jxavierdefarias@byupathway.edu>
2026-09-12 20:54:35 -0300

    alt2

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,8 +1,8 @@
-// Rodapé dinâmico igual da semana passada
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Última modificação: ${document.lastModified}`;
 
-// Menu hamburguer
+
 const menuButton = document.getElementById("menu");
 const nav = document.querySelector("nav");
 menuButton.addEventListener("click", () => {
