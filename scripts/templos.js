5ff53fd8ce636d03bf41a97da0dcba8e37a208e8
jxavierdefarias <jxavierdefarias@byupathway.edu>
2026-09-13 20:17:40 -0300

    alteração 6

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,15 +1,10 @@
 
-const year = document.querySelector("#currentyear");
-const lastMod = document.querySelector("#lastModified");
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Ultima Modificação: ${document.lastModified}`;
 
-year.textContent = new Date().getFullYear();
-lastMod.textContent = `Last Modification: ${document.lastModified}`;
-
-
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-hamButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    hamButton.classList.toggle("open");
+const btn = document.getElementById("menu");
+const nav = document.querySelector(".navigation");
+btn.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    btn.classList.toggle("open");
 });
