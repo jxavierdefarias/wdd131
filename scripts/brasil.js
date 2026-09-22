e5d2f71a139014af95081ceaec1ee93b96db9e89
jxavierdefarias <jxavierdefarias@byupathway.edu>
2026-09-21 23:13:35 -0300

    sensação termica

--- scripts/brasil.js
+++ scripts/brasil.js
@@ -3,7 +3,7 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
 
-// Sensação Térmica mesmo com calor
+
 function calculateWindChill(tempC, windKmh) {
     let wc = 13.12 + 0.6215 * tempC - 11.37 * Math.pow(windKmh, 0.16) + 0.3965 * tempC * Math.pow(windKmh, 0.16);
     return wc.toFixed(1) + " °C";
